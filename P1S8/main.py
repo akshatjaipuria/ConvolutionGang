76ee2a6f1db0241ddf2d401c4a40ac65beda3973
Akshat Jaipuria <38358340+akshatjaipuria@users.noreply.github.com>
2020-03-14 21:53:59 +0530

    Update main.py

--- P1S8/main.py
+++ P1S8/main.py
@@ -7,5 +7,5 @@ trainloader, testloader, classes = data_loader.cifar10()
 net = ResNet18()
 device = model_summary(net, (3, 32, 32))
 trainer = train_model(trainloader, testloader, device)
-trainer.run_model(net=net, epochs=20, l2=0.00001, gamma=0.1)
+trainer.run_model(net=net, epochs=20, l2=0.00001, gamma=0.5)
 
